be335a2623aa995aad9e326595236ba30f1a608d
Victor Chaves Moreira <101597103+VictorMoreira97@users.noreply.github.com>
2023-08-24 09:37:52 -0300

    m

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,15 @@
 <php? 
-echo "Hello World";
-?>
+    echo <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Aula 1</title>
+    </head>
+    <body>
+        <h1>
+            Pokemon: gotta cacth them all!
+        </h1>
+    </body>
+    </html>
+
