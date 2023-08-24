d84e734355737a8fde39471dca0f7d6c33236140
Victor Chaves Moreira <101597103+VictorMoreira97@users.noreply.github.com>
2023-08-24 10:08:17 -0300

    m

---
+++ PHP/index02.php
@@ -0,0 +1,17 @@
+<?php
+$nomeDaVariavel = 10;
+
+var_dump($nomeDaVariavel);
+
+$nomeDaVariavel = 10.0;
+
+echo "<br>";
+
+var_dump($nomeDaVariavel);
+
+$nomeDaVariavel = "String";
+
+echo "<br>";
+var_dump($nomeDaVariavel);
+
+?>
